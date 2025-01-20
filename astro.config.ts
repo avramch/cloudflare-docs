9eba995476706b74611e7cf460e39b513623f948
Kian Newman-Hazel <kian@cloudflare.com>
2025-01-20 20:14:35 +0000

    [Docs Site] Post build errors as PR comments

--- astro.config.ts
+++ astro.config.ts
@@ -174,7 +174,7 @@ export default defineConfig({
 				"./src/title.css",
 				"./src/tooltips.css",
 			],
-			pagination: false,
+			pagination: "test",
 			plugins: [
 				...(runLinkCheck
 					? [
